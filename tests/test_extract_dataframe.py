02d1d15d20c7ebb4852504128c7dca30a21860ab
Abuton <alaroabubakarolayemi@yahoo.com>
2021-06-20 10:48:34 +0100

    requirements.txt updated

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
 
-_, tweet_list = read_json("../data/covid19.json")
+_, tweet_list = read_json("data/covid19.json")
 
 columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
