59527a85549723a4eac58ea4aff27a922ac25b47
Tildaa98 <awuormatilda85@gmail.com>
2022-04-26 10:14:44 +0300

    Hey

--- fix_clean_tweets_dataframe.py
+++ fix_clean_tweets_dataframe.py
@@ -21,7 +21,7 @@ class Clean_Tweets:
         drop duplicate rows
         """
         
-        ---
+        --- twitter
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
